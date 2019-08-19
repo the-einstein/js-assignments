60fac7a2c5fff4ba3885408fb382b8ce4b3b1adb
Shamsiddin Avezov <quyosh.ogli@gmail.com>
2019-08-19 16:36:36 +0500

    feat: add sutes for Array functions and Object functions

--- custom-lodash-tests/spec/MyJSUtilitiesSpec.js
+++ custom-lodash-tests/spec/MyJSUtilitiesSpec.js
@@ -1,7 +1,86 @@
 describe("MyJSUtilities", function() {
     
     
+    describe("Array Utils", function() {
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Compact",function() {
+            expect().nothing();
+          });
+          it("Test for Drop",function() {
+            expect().nothing();
+          });
+          it("Test for DropWhile",function() {
+            expect().nothing();
+          });
+          it("Test for Take",function() {
+            expect().nothing();
+          });
+          it("Test for Filter",function() {
+            expect().nothing();
+          });
+          it("Test for Find",function() {
+            expect().nothing();
+          });
+          it("Test for Includes",function() {
+            expect().nothing();
+          });
+          it("Test for Map",function() {
+            expect().nothing();
+          });
+          it("Test for Zip",function() {
+            expect().nothing();
+          });
+       });
+       
 
+
+       describe("Object Utils", function() {
+        it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+          it("Test for Chunk",function() {
+            expect().nothing();
+          });
+       });
+       
 
    });
    
